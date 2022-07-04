f7061e38b0058392954f83ac051e7005918cf72e
tascintra <tiago.slv@hotmail.com>
2022-07-04 18:04:37 -0300

    created extra exs class 12

---
+++ aula12/ex-hora/script.js
@@ -0,0 +1,26 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var sau = window.document.getElementById('saudacao')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if ( hora >= 0 && hora < 12) {
+        // BOM DIA!
+        img.src = 'foto-manha.png'
+        document.body.style.background = '#e2cd9f'
+        sau.innerText = "Bom dia!"
+    } else if (hora >= 12 && hora <= 18) {
+        // BOA TARDE!
+        img.src = 'foto-tarde.png'
+        document.body.style.background = '#b9846f'
+        sau.innerText = "Boa tarde!"
+    } else {
+        // BOA NOITE!
+        img.src = 'foto-noite.png'
+        document.body.style.background = '#2c2327'
+        sau.innerText = "Boa noite!"
+    }
+}
+
+
---
+++ aula12/ex-idade/script.js
@@ -0,0 +1,62 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fAno = document.querySelector('input#txtano')
+    var res = document.getElementById('res')
+    
+    if (fAno.value.length == 0 || Number(fAno.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var rSex = document.getElementsByName('radsex')
+        var idade = ano - Number(fAno.value)
+        var genero = ''
+
+        // COLOCANDO IMAGEM NA PAGINA EM JS
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+
+        if (rSex[0].checked) {
+            if (idade >= 0 && idade <= 10) {
+                // CRIANÇA
+                genero = 'criança'
+                img.setAttribute('src', 'foto-bebe-m.png')
+            } else if (idade < 21) {
+                // JOVEM
+                img.setAttribute('src', 'foto-jovem-m.png')
+                genero = 'jovem'
+            } else if (idade < 50) {
+                // ADULTO
+                img.setAttribute('src', 'foto-adulto-m.png')
+                genero = 'homem'
+            } else {
+                // IDOSO
+                img.setAttribute('src', 'foto-idoso-m.png')
+                genero = 'idoso'
+            }
+
+        } else {
+            if (idade >= 0 && idade <= 10) {
+                // CRIANÇA
+                img.setAttribute('src', 'foto-bebe-f.png')
+                genero = 'criança'
+            } else if (idade < 21) {
+                // JOVEM
+                img.setAttribute('src', 'foto-jovem-f.png')
+                genero = 'jovem'
+            } else if (idade < 50) {
+                // ADULTA
+                img.setAttribute('src', 'foto-adulto-f.png')
+                genero = 'mulher'
+            } else {
+                // IDOSA
+                img.setAttribute('src', 'foto-idoso-f.png')
+                genero = 'idosa'
+            }
+        }
+        // res.style.textAlign = 'Center' // CENTRALIZA O TEXTO COM JS
+        res.innerHTML = `Detectamos <strong>${genero}</strong> com ${idade} anos.`
+        res.appendChild(img)
+        img.style.padding = '10px 1px 4px 1px'
+    }
+}
